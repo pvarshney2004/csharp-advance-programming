d64d96d7e2641ddb6e671de20dd7c040eb8dab2b
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-19 12:31:33 +0530

    [Prashant] add Program.cs file

---
+++ Lambda-LINQ/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Lambda.EmployeeManagement;
+using Lambda.ProductManagement;
+using Lambda.StudentSorting;
+namespace Lambda
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // EmployeeMain.Execute();
+
+            // SortStringsUsingLINQ.Execute();
+
+            // ProductMain.Execute()
+
+            // SortStrings.Execute();
+
+            // StudentSortingMain.ExecuteUsingComparer();
+            // StudentSortingMain.ExecuteUsingLambda();
+
+            ProductMain.Execute();
+
+            
+        }
+    }
+}
