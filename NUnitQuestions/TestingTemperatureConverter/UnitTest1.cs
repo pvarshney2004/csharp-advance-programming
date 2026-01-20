a709ad1b4bcde07c0e88b6ccaf429a58282e445b
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-20 19:35:34 +0530

    [Prashant] add program for testing temperature converter using NUnit testing

---
+++ NUnitQuestions/TestingTemperatureConverter/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace TestingTemperatureConverter;
+
+public class Tests
+{
+    TemperatureConverter tc;
+    [SetUp]
+    public void Setup()
+    {
+        tc = new TemperatureConverter();
+    }
+
+    // test cases
+    [Test]
+    public void Test_For_Cel_To_Farh()
+    {
+        Assert.That(tc.CelsiusToFahrenheit(0), Is.EqualTo(32));
+    }
+    [Test]
+    public void Test_For_Farh_To_Cel()
+    {
+        Assert.That(tc.FahrenheitToCelsius(32), Is.EqualTo(0));
+    }
+}
