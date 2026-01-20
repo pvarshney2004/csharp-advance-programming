3cef96899d94477cb028fad9656607e502d05391
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-20 19:32:40 +0530

    [Prashant] add program for testing setup and teardown using NUnit testing

---
+++ NUnitQuestions/TestingSetUpAndTearDown/DatabaseConnection.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingSetUpAndTearDown
+{
+    /*
+    5. Testing Setup and Teardown (NUnit: [SetUp] & [TearDown])
+Problem:
+Create a class DatabaseConnection with methods:
+● Connect()
+● Disconnect()
+Use [SetUp] (NUnit) or [TestInitialize] (MSTest) to initialize a database
+connection before each test.
+Use [TearDown] (NUnit) or [TestCleanup] (MSTest) to close the connection after
+each test.
+Write test cases to verify that the connection is established and closed
+correctly.
+    */
+    public class DatabaseConnection
+    {
+        // method to connect database
+        public static void Connect()
+        {
+            System.Console.WriteLine("Database Connected");
+        }
+        // method to disconnect database
+        public static void Disconnect()
+        {
+            System.Console.WriteLine("Database disconnected");
+        }
+    }
+}
---
+++ NUnitQuestions/TestingSetUpAndTearDown/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿namespace TestingSetUpAndTearDown;
+
+public class Tests
+{
+    // runs before each test case
+    [SetUp]
+    public void Setup()
+    {
+        DatabaseConnection.Connect();
+    }
+
+    // test cases
+    [Test]
+    public void Test1()
+    {
+        System.Console.WriteLine("Testing case 1...");
+    }
+    [Test]
+    public void Test2()
+    {
+        System.Console.WriteLine("Testing case 2...");
+    }
+
+    // runs after each test case
+    [TearDown]
+    public void TearDown()
+    {
+        DatabaseConnection.Disconnect();
+    }
+}
