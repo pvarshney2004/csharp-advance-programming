bc5ebfecb1102320b2666c00794bc395aad92b54
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-20 19:34:18 +0530

    [Prashant] add program for performance testing using timeout by Nunit

---
+++ NUnitQuestions/TestingTimeout/Timeout.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingTimeout
+{
+    public class Timeout
+    {
+        /*
+        7. Performance Testing Using Timeout
+Problem:
+Create a method LongRunningTask() that sleeps for 3 seconds before returning
+a result.
+Use NUnit [Timeout(2000)] or MSTest [Timeout(2000)] to fail the test if the
+method takes more than 2 seconds.
+        */
+
+        public string LongRunningTask()
+        {
+            Thread.Sleep(3000); // sleeps for 3 seconds
+            return "Completed";
+        }
+    }
+}
---
+++ NUnitQuestions/TestingTimeout/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace TestingTimeout;
+
+public class Tests
+{
+    [Test]
+    [Timeout(2000)] // 2 seconds
+    public void LongRunningTask_ShouldFail()
+    {
+        Timeout task = new Timeout();
+        Assert.That(task.LongRunningTask(), Is.EqualTo("Completed"));
+    }
+}
