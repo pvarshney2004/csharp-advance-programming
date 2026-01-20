39d54568718c3fd0bcece683bb68e753df1d6ca8
Prashant Varshney <prashantvarshney4321@gmail.com>
2026-01-20 19:33:03 +0530

    [Prashant] add program for testing parameterized tests using NUnit testing

---
+++ NUnitQuestions/TestingParameterizedTests/NumberCheck.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestingParameterizedTests
+{
+    public class NumberCheck
+    {
+        // method to check if a number is even or not
+        public bool IsEven(int number)
+        {
+            return number%2==0;
+        }
+    }
+}
---
+++ NUnitQuestions/TestingParameterizedTests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿namespace TestingParameterizedTests;
+
+public class Tests
+{
+    /*
+    6. Testing Parameterized Tests
+Problem:
+Create a method IsEven(int number) that returns true if a number is even.
+Use NUnit [TestCase] or MSTest [DataRow] to test this method with multiple
+values like 2, 4, 6, 7, 9
+    */
+
+    [TestCase(2, true)]
+    [TestCase(4, true)]
+    [TestCase(6, true)]
+    [TestCase(7, true)]
+    [TestCase(9, true)]
+    public void IsEven_TestCases(int input, bool exp)
+    {
+        NumberCheck nc = new NumberCheck();
+        Assert.That(nc.IsEven(input), Is.EqualTo(exp));
+    }
+}
